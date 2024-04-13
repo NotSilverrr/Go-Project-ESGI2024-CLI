7e6d136ab4a8e7aa5a8930bc6b9164ee11dbcec4
Koupy <nanook.tech@proton.me>
2024-04-13 20:36:43 +0200

    add Salle package with view function

---
+++ salle/salle.go
@@ -0,0 +1,29 @@
+package salle
+
+// import (
+// 	"fmt"
+// 	"log"
+// )
+
+//  type Salle struct {
+//  	capacity int
+//  	name     string
+//  	booked   bool
+//  }
+
+//  func Affiche() {
+//  		rows, err := db.Query("Select name FROM salles WHERE booked = 0")
+
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		defer rows.Close()
+
+// 		for rows.Next() {
+// 			var name string
+// 			if err := rows.Scan(&name); err != nil {
+// 				log.Fatal(err)
+// 			}
+// 			fmt.Println(name)
+// 		}
+//  }
