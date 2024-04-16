a60c1e07a826c24a680abcf513f204bb04b62421
Tomate <tronsin7@gmail.com>
2024-04-16 14:56:21 +0200

    add display reservation

--- booking/booking.go
+++ booking/booking.go
@@ -63,4 +63,33 @@ func CancelReservation(id int, db *sql.DB) {
 	}
 }
 
-func DisplayReservation() {}
+func DisplayReservation(roomID int, db *sql.DB) {
+	var name string
+	var id string
+	var date_start string
+	var date_end string
+	var time_start string
+	var time_end string
+	rows, err := db.Query("Select id,date_start,date_end,time_start,time_end FROM réservation WHERE id_salle=?", roomID)
+	rowsRoom, err := db.Query("Select name FROM room WHERE id=?", roomID)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = rows.Scan(&name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	println("Réservations pour ", name)
+	for rows.Next() {
+		err = rowsRoom.Scan(&id, &date_start, &date_end, &time_start, &time_end)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		println(id, ".", date_start, time_start, " - ", date_end, time_end)
+	}
+
+}
