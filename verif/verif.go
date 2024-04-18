33514fbbcc069ed5257218b59c47d9495566e10e
Koupy <nanook.tech@proton.me>
2024-04-18 12:39:39 +0200

    add isDateLogic function in verif.go

--- verif/verif.go
+++ verif/verif.go
@@ -11,6 +11,12 @@ func VerifDate(date string) string {
 	return "ok"
 }
 
+func isDateLogic(day int, month int, year int) string{
+	monthDays := [][]int{{1, 31}, {2, 29}, {3, 31}, {4, 30}, {5, 31}, {6, 30}, {7, 31}, {8, 31}, {9, 30}, {10, 31}, {11, 30}, {12, 31}}
+	println(monthDays)
+	return "ok"
+}
+
 func VerifTime(hour string) string {
 	correctHour := regexp.MustCompile(`^\d{2}:\d{2}$`)
 	if !correctHour.MatchString(hour) {
