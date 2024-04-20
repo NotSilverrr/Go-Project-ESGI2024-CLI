625728ba119602525f83e88c16501ae36a609a65
Koupy <nanook.tech@proton.me>
2024-04-20 12:16:54 +0200

    add past verification in verif.go
    Also add red color on error messages

--- verif/verif.go
+++ verif/verif.go
@@ -8,13 +8,13 @@ import (
 func VerifDate(date string) string {
 	correctDate := regexp.MustCompile(`^\d{2}-\d{2}-\d{4}$`)
 	if !correctDate.MatchString(date) {
-		err := "Format de date invalide. Veuillez entrer une date au format JJ-MM-AAAA."
+		err := "\033[31mFormat de date invalide. Veuillez entrer une date au format JJ-MM-AAAA.\033[0m"
 		return err
 	}
 	return "ok"
 }
 
-func IsDateLogic(day int, month int, year int) string {
+func IsDateLogic(day, month, year int) string {
 	monthDays := [][]int{{1, 31}, {2, 28}, {3, 31}, {4, 30}, {5, 31}, {6, 30}, {7, 31}, {8, 31}, {9, 30}, {10, 31}, {11, 30}, {12, 31}}
 
 	//leap year
@@ -23,23 +23,34 @@ func IsDateLogic(day int, month int, year int) string {
 	} 
 
 	if month < 1 || month > 12{
-		err := "Le mois choisi n'est pas valide !"
+		err := "\033[31mLe mois choisi n'est pas valide !\033[0m"
 		return err
 	}
 
 	if day < 1 || day > monthDays[month-1][1]{
-		err := "Le jour choisi n'est pas valide !"
+		err := "\033[31mLe jour choisi n'est pas valide !\033[0m"
 		return err
 	}
 	return "ok"
 }
 
-func IsBookLogic(startDay int, startMonth int, startYear int, endDay int, endMonth int, endYear int) string {
+func IsBookingDayInPast(startDay, startMonth, startYear int) string {
+	currentDate := time.Now().UTC()
+	startDate := time.Date(startYear, time.Month(startMonth), startDay, 0, 0, 0, 0, time.UTC)
+
+	if startDate.Before(currentDate) {
+		err := "\033[31mLa date de début de réservation ne peut pas être dans le passé.\033[0m"
+		return err
+	}
+	return "ok"
+}
+
+func IsBookingLogic(startDay, startMonth, startYear, endDay, endMonth, endYear int) string {
 	startDate := time.Date(startYear, time.Month(startMonth), startDay, 0, 0, 0, 0, time.UTC)
   endDate := time.Date(endYear, time.Month(endMonth), endDay, 0, 0, 0, 0, time.UTC)
 
 	if endDate.Before(startDate) {
-		err := "La date de fin ne peut pas être antérieure à la date de début."
+		err := "\033[31mLa date de fin ne peut pas être antérieure à la date de début.\033[0m"
 		return err
 	}
 	return "ok"
@@ -48,7 +59,7 @@ func IsBookLogic(startDay int, startMonth int, startYear int, endDay int, endMon
 func VerifTime(hour string) string {
 	correctHour := regexp.MustCompile(`^\d{2}:\d{2}$`)
 	if !correctHour.MatchString(hour) {
-		err := "Format d'heure invalide. Veuillez entrer une heure au format HH:MM."
+		err := "\033[31mFormat d'heure invalide. Veuillez entrer une heure au format HH:MM.\033[0m"
 		return err
 	}
 	return "ok"
